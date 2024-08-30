9b55459f72245c646477f3cbdb9427072a62627a
wldlsgur <dhgg321@naver.com>
2024-08-31 03:36:32 +0900

    🌱feat: sever cors 정책 추가

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import * as Router from '@/routes';
 
 const app: Express = express();
 const port = 5000;
+
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 app.use('/api/v1/users', Router.UserRouter);
 app.use('/api/v1/rooms', Router.RoomRouter);
