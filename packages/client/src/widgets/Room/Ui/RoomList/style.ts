76ee53f4af134543668e401d0e2bdebc7a3cab3c
wldlsgur <dhgg321@naver.com>
2024-09-02 00:36:44 +0900

    🎨style: 방 목록 스타일 수정

--- packages/client/src/widgets/Room/Ui/RoomList/style.ts
+++ packages/client/src/widgets/Room/Ui/RoomList/style.ts
@@ -5,7 +5,35 @@ export const RoomList = styled.ul`
   display: flex;
   flex-direction: column;
   flex-wrap: nowrap;
-  gap: 3rem;
-  padding: 3rem;
+  margin: 3rem 3rem;
   overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 1rem;
+    padding: 0;
+    margin: 0;
+    border: none;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+    border-radius: 50%;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.colors.primaryLight};
+    border-radius: 0.5rem;
+    transition: all 1s;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: ${({ theme }) => theme.colors.primaryNormal};
+  }
+`;
+
+export const EmptyContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
