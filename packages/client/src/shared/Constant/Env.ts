1b4cb7d7d7a740a668aeaea22b35cf0630f75336
wldlsgur <dhgg321@naver.com>
2024-09-03 03:47:18 +0900

    🌱feat: client env 추가

--- packages/client/src/shared/Constant/Env.ts
+++ packages/client/src/shared/Constant/Env.ts
@@ -1,5 +1,7 @@
 const ENV = {
   API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+  KAKAO_REST_API_KEY: import.meta.env.VITE_KAKAO_REST_API_KEY,
+  KAKAO_REDIRECT_URL: import.meta.env.VITE_KAKAO_REDIRECT_URL,
 };
 
 export default ENV;
