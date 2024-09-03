5e87beeb056a7155882a3dd98583566d90879b29
wldlsgur <dhgg321@naver.com>
2024-09-04 00:47:32 +0900

    🐞fix: AxiosInstance 쿠키 허용 설정

--- packages/client/src/shared/Config/AxiosInstance.ts
+++ packages/client/src/shared/Config/AxiosInstance.ts
@@ -4,6 +4,7 @@ import { ENV } from '@/shared/Constant';
 const axiosInstance = axios.create({
   baseURL: ENV.API_BASE_URL,
   timeout: 10000,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
