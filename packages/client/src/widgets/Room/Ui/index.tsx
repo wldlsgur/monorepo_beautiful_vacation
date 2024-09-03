1ec9edb9ec8931d5beee61cc7ce4cfa3bd1e5beb
wldlsgur <dhgg321@naver.com>
2024-09-04 02:18:05 +0900

    🏷️rename: 대소문자 변경

--- packages/client/src/widgets/Room/Ui/index.tsx
+++ packages/client/src/widgets/Room/Ui/index.tsx
@@ -1,3 +1,3 @@
 export { default as Header } from './Header';
 export { default as SearchBar } from './searchBar';
-export { default as RoomList } from './RoomList';
+export { default as RoomList } from './roomList';
---
+++ packages/client/src/widgets/Room/Ui/roomList/index.tsx
@@ -0,0 +1,77 @@
+import { useEffect } from 'react';
+import { InfiniteData } from '@tanstack/react-query';
+import { Room, RoomListResponse } from 'common-types';
+import { Skeleton } from 'jiponent';
+import { useInView } from 'react-intersection-observer';
+import Lottie from 'react-lottie-player';
+import { EMPTY_LOTTIE } from '@/shared/lottie';
+import RoomItem from '../RoomItem';
+import * as S from './style';
+
+interface Props {
+  data: InfiniteData<RoomListResponse> | undefined;
+  hasNextPage: boolean;
+  isFetching: boolean;
+  fetchNextPage: () => void;
+}
+
+const RoomList = ({ data, hasNextPage, isFetching, fetchNextPage }: Props) => {
+  const [ref, inView] = useInView({ threshold: 0.5 });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, hasNextPage, inView, isFetching]);
+
+  if (!data || data.pages[0].data.length === 0) {
+    return (
+      <S.EmptyContainer>
+        <Lottie
+          animationData={EMPTY_LOTTIE}
+          play
+          loop
+          speed={0.5}
+          style={{ width: 200, height: 200 }}
+        />
+      </S.EmptyContainer>
+    );
+  }
+
+  return (
+    <S.RoomList>
+      {data?.pages.map((page, pageIndex) =>
+        page.data.map((room: Room, roomIndex) => {
+          const isLast =
+            pageIndex === data.pages.length - 1 &&
+            roomIndex === page.data.length - 1;
+
+          return (
+            <RoomItem
+              key={room.room_id}
+              roomData={room}
+              ref={isLast ? ref : null}
+            />
+          );
+        }),
+      )}
+      {isFetching &&
+        Array.from({ length: 9 }, (_, index) => (
+          <S.LoadingItem key={index}>
+            <Skeleton.Paragraph
+              line={1}
+              height='1.5rem'
+              style={{ flexBasis: '80%' }}
+            />
+            <Skeleton.Paragraph
+              line={1}
+              height='1.5rem'
+              style={{ flexBasis: '10%' }}
+            />
+          </S.LoadingItem>
+        ))}
+    </S.RoomList>
+  );
+};
+
+export default RoomList;
