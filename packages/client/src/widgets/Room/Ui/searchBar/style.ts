d6e664bc8ae4c00332b40a28c15ce2f0382ed254
wldlsgur <dhgg321@naver.com>
2024-09-04 02:17:44 +0900

    🏷️rename: 대소문자 변경

---
+++ packages/client/src/widgets/Room/Ui/searchBar/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  width: 100%;
+  min-width: 30rem;
+  max-width: 50%;
+  margin: 0 auto;
+`;
