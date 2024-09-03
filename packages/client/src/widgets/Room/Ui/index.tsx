ee7edf66fdff4efb5ef33dfdf63fc5f4db042e67
wldlsgur <dhgg321@naver.com>
2024-09-04 02:24:38 +0900

    🏷️rename: 대소문자 변경

--- packages/client/src/widgets/Room/Ui/index.tsx
+++ packages/client/src/widgets/Room/Ui/index.tsx
@@ -1,3 +1,3 @@
-export { default as Header } from './Header';
+export { default as Header } from './header';
 export { default as SearchBar } from './searchBar';
 export { default as RoomList } from './roomList';
