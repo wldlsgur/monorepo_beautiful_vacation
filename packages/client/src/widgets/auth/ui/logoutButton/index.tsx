a1c066015295d40262121780a70ee2f3cdc27410
wldlsgur <dhgg321@naver.com>
2024-09-09 19:22:22 +0900

    🛠️refactor: 로그아웃 버튼 컴포넌트 코드 개선

--- packages/client/src/widgets/auth/ui/logoutButton/index.tsx
+++ packages/client/src/widgets/auth/ui/logoutButton/index.tsx
@@ -1,19 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { MUTATE_OPTION, QUERY_KEY } from '@/shared/constant';
+import { MUTATE_OPTION } from '@/shared/constant';
 import { Button } from '@/shared/ui';
 
 const LogoutButton = () => {
   const queryClient = useQueryClient();
-  const { mutate } = useMutation(MUTATE_OPTION.LOGOUT());
+  const { mutate } = useMutation(MUTATE_OPTION.LOGOUT({ queryClient }));
 
-  const handleLogout = async () => {
-    mutate(undefined, {
-      onSuccess: () =>
-        queryClient.invalidateQueries({ queryKey: QUERY_KEY.AUTH }),
-    });
-  };
-
-  return <Button onClick={handleLogout}>로그아웃</Button>;
+  return <Button onClick={() => mutate()}>로그아웃</Button>;
 };
 
 export default LogoutButton;
