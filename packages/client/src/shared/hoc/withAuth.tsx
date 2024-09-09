2e141b9bab2e22a0ae9a491db2f79c5f5de042fa
wldlsgur <dhgg321@naver.com>
2024-09-09 21:03:14 +0900

    🐞fix: 인증 확인 api 에러 시 null 캐싱 안되는 버그 수정

--- packages/client/src/shared/hoc/withAuth.tsx
+++ packages/client/src/shared/hoc/withAuth.tsx
@@ -1,15 +1,22 @@
-import { ElementType } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { QUERY_OPTION } from '@/shared/constant';
+import { ElementType, useEffect } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { QUERY_KEY, QUERY_OPTION } from '@/shared/constant';
 
 const withAuth = <P extends object>(
   WrappedComponent: ElementType,
   FallbackComponent: ElementType,
 ) => {
   const WithAuthComponent = (props: P) => {
+    const queryClient = useQueryClient();
     const { data, isError } = useQuery(QUERY_OPTION.AUTH());
     const extendedProps = { ...props, user: data?.data };
     const isLogin = data?.data && !isError;
+
+    useEffect(() => {
+      if (isError) {
+        queryClient.setQueryData(QUERY_KEY.AUTH, { data: null });
+      }
+    }, [isError, queryClient]);
 
     return isLogin ? (
       <WrappedComponent {...extendedProps} />
