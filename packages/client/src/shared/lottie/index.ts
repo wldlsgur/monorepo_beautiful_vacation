59a34b7d4696703bec506069a899befa6daa16b6
wldlsgur <dhgg321@naver.com>
2024-09-12 01:03:03 +0900

    🌱feat: spinner lottie 추가

--- packages/client/src/shared/lottie/index.ts
+++ packages/client/src/shared/lottie/index.ts
@@ -1,3 +1,4 @@
 export { default as EMPTY_LOTTIE } from './empty.json';
 export { default as KAKAO_LOANING_LOTTIE } from './kakaoLoading.json';
 export { default as LOGO_LOTTIE } from './logo.json';
+export { default as SPINNER_LOTTIE } from './spinner.json';
