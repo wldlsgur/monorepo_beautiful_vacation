b48ae815e9e4401b1228f767224f5ac2c974d41b
wldlsgur <dhgg321@naver.com>
2024-10-16 18:55:58 +0900

    🐞fix: redis host, port 설정

--- packages/server/src/config/redis.ts
+++ packages/server/src/config/redis.ts
@@ -1,6 +1,10 @@
 import Redis from 'ioredis';
+import CONFIG from './config';
 
-const redisClient = new Redis();
+const redisClient = new Redis({
+  host: CONFIG.REDIS.host,
+  port: Number(CONFIG.REDIS.port),
+});
 
 redisClient.on('error', (error) => {
   console.log('Redis error:', error);
